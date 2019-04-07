7fad9b9486b5ca6cb467134051eb6655c24f6f08
mkierin <mkierin@gmail.com>
2019-04-07 15:18:55 +0200

    Add files via upload
    corrected length of the array to be checked

--- bubblesort.php
+++ bubblesort.php
@@ -5,11 +5,11 @@
 $numbers = array(4,5,6,7,4,3,4,5,6,7,8,9,87,65,43,23,12,34);
 
 
-$length = count($numbers);
+$length = count($numbers)-1;
 $a=0;
 $temp=0;
 
-while ($a <= $length) {
+while ($a < $length) {
 
 if ($numbers[$a]>$numbers[$a+1]) {
   //swap places of the two array values
