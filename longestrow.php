83008004f4a37ae2d3aa6c8a9830bbf134432eb3
mkierin <mkierin@gmail.com>
2019-04-23 20:23:08 +0200

    Add files via upload

--- longestrow.php
+++ longestrow.php
@@ -1,34 +1,33 @@
 <?php
 
-// Aufgabe 2
-//
-// Implementieren Sie eine Funktion laengsteReihe, die die Länge der längsten Reihe von gleichen Elementen in einem Array  zurückliefert. Eine Reihe ist eine Sequenz von 2 oder mehreren Elemente mit dem gleichen Wert, die nebeneinander liegen.
-//
-//  Beispiele
-//
-// Array List                         Rückgabewert der Methode
-//
-// (3 7 5)                                                   1
-//
-$zahlen1 = array(4,4,3,5,6,7,4,4);
-$zahlen2 = array(1,2,3,4,5,5,5,6,7,8,9);
 
-$zahlen = $zahlen2;
+$zahlen1 = array(4,4,3,5,6,7,4,4); // answer: 2 44
+$zahlen2 = array(1,2,6,6,3,4,5,5,5,6,7,8,6,9); // answer: 3 555
+$zahlen3 = array(4,4,5,5,3,2,1,7,7,7,7,4,4,5 ); // answer: 4  7777
+// compare value 1 with the next value in line. If the same amount +1 of len
+// if the len of the row is longer than the previous row then new flag amount
+
+$zahlen = $zahlen3;
 
 echo longestrow($zahlen);
 
-function longestrow($zahlen){
-  $amount = 0;
-  $flag = 0;
-foreach ($zahlen as $value) {
-    foreach ($zahlen as $value2) {
-        if ($value === $value2) {
+function longestrow($zahlen)
+{
+    $amount = 1;
+    $flag = 0;
+
+    for ($a=0; $a < count($zahlen) ; $a++) {
+        if ($zahlen[$a] === $zahlen[$a+1]) {
             $amount++;
+
 
             if ($amount > $flag) {
                 $flag = $amount;
             }
         }
-    } $amount = 0;
-} return $flag;
+        if ($zahlen[$a] !== $zahlen[$a+1]) {
+            $amount = 1;
+        }
+    }
+    return $flag;
 }
